8e153390ce343456b61313a07c186d75891136dc
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-05 06:35:22 -0800

    learning the min,max,sum,avg,count functions

---
+++ aggregrate functions.sql
@@ -0,0 +1,12 @@
+use dummy;
+create table data(
+name varchar(1000),
+mark int,
+department varchar(1000)
+);
+insert into data values("karthi",1,"ECE");
+insert into data values("manoj",2,"CSE");
+insert into data values("nivya",3,"ECE");
+insert into data values("munivesh",4,"CSE");
+select *from data;
+

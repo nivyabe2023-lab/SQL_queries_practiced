49e22df39ee89d726f8244f05c741fbe6bd4fecd
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-06 08:56:00 -0800

    groupby and orderby

---
+++ groupby and order by practise question.sql
@@ -0,0 +1,16 @@
+create table Employeess(
+Employee_id int,
+Employee_firstname varchar(100),
+Employee_lastname varchar(100),
+department varchar(100),
+salary int
+);
+insert into Employeess(Employee_id,Employee_firstname,Employee_lastname,department,salary)
+values
+(1,'Jhon','Doe','MR',55000),
+(2,'Jane','Smith','IT',60000),
+(3,'Bob','Johnson','IT',62000),
+(4,'Alice','Willians','HR',54000),
+(5,'Eva','Davis','Finance',58000),
+(6,'Mike','Brown','Finance',59000);
+select *from Employeess
